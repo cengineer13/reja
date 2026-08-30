da1b86f4833b41acfa9ef94859d1cfa3ce7093bf
cengineer13 <murodild@gmail.com>
2026-08-30 15:41:03 +0500

    vazifa: Asynchronous functionlarni qo'llash

--- train.js
+++ train.js
@@ -8,6 +8,7 @@ const list = [
     "Endi dam oling. Foydasi yo'q baribir" // 60
 ]
 
+console.log("================= CALLBACK FUNKSIYA =================")
 function maslahatBering(a, callback) {
     if (typeof a !== 'number' || a <= 0) callback("insert a proper number", null);
     else if (a > 0 && a <= 20) callback(null, list[0]);
@@ -18,11 +19,11 @@ function maslahatBering(a, callback) {
     else {
         setTimeout(()=> {
             callback(null, list[5]);
-        }, 3000);
+        }, 2000);
     }
 };
 console.log("Passed 1")
-maslahatBering(65, (err, data) => {
+maslahatBering(31, (err, data) => {
     if (err) console.log("Error:", err);
     else {
         console.log("Data:", data)
@@ -31,9 +32,10 @@ maslahatBering(65, (err, data) => {
 console.log("Passed 2")
 
 
-/////////////////-------------------------A-TASK -------------------------/////////////////
-/* Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
-MASALAN countLetter("e", "engineer") 3ni return qiladi. */
+// /////////////////-------------------------A-TASK -------------------------/////////////////
+console.log("================= A-TASK =================")
+// /* Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+// MASALAN countLetter("e", "engineer") 3ni return qiladi. */
 const result = (letter, word) => {
     let count = 0;
     for (let i = 0; i <= word.length; i++) {
@@ -43,3 +45,70 @@ const result = (letter, word) => {
     return count;
 };
 console.log(result("e", "engineer"));
+
+
+
+/////////////////-------------------------Asynchronous functionlarni qo'llash -------------------------/////////////////
+console.log("================= ASYNCHRONOUS FUNKSIYA =================")
+async function maslahatBering2(a) {
+    if (typeof a !== 'number' || a <= 0) throw new Error("insert a proper number");
+    else if (a > 0 && a <= 20) return list[0];
+    else if (a >= 21 && a <= 30) return list[1];
+    else if (a >= 31 && a <= 40) return list[2];
+    else if (a >= 41 && a <= 50) return list[3];
+    else if (a >= 51 && a <= 60) return list[4];
+    else { 
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(list[5]);
+            }, 5000); 
+        });
+};
+};
+
+// Call via then/catch 
+console.log("Passed 1")
+maslahatBering2(25).then((data) => {
+    console.log('Then dagi javob:', data);
+
+}).catch((err) => {
+    console.log("xatolik:", err);
+})
+console.log("Passed 2")
+
+// Call via asyn / await 
+async function run() {
+    let javob = await maslahatBering2(20);
+    console.log(javob);
+    javob = await maslahatBering2(31);
+    console.log(javob);
+    javob = await maslahatBering2(70);
+    console.log(javob);
+    javob = await maslahatBering2(52);
+    console.log(javob);
+};
+run()
+
+// SET interval va SetTime out funksiyalarni callback bilan ishlatilishi
+
+function maslahatBering3(a, callback) {
+    if (typeof a !== 'number' || a <= 0) callback("insert a proper number", null);
+    else if (a > 0 && a <= 20) callback(null, list[0]);
+    else if (a >= 21 && a <= 30) callback(null, list[1]);
+    else if (a >= 31 && a <= 40) callback(null, list[2]);
+    else if (a >= 41 && a <= 50) callback(null, list[3]);
+    else if (a >= 51 && a <= 60) callback(null, list[4]);
+    else {
+        setInterval(()=> {
+            callback(null, list[5]);
+        }, 1000);
+    }
+};
+console.log("Passed 1")
+maslahatBering3(65, (err, data) => {
+    if (err) console.log("Error:", err);
+    else {
+        console.log("Callback via setInterval:", data)
+    }
+});
+console.log("Passed 2")
